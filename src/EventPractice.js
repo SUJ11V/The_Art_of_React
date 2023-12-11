d09dd19e933e1eef76165c8351d5b0dcd6f47d30
SUJ11V <ejqw12@naver.com>
2023-12-12 03:16:49 +0900

    [4.3] 함수형 컴포넌트로 구현해보기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,56 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EventPractice extends Component {
-  state = {
-    message: "",
-    usernname: "",
+const EventPractice = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ": " + message);
+    setUsername("");
+    setMessage("");
   };
-
-  handleChange = (e) => {
-    this.setState({
-      // e.target.name을 활용하여 input이 여러 개지만, 메서드를 여러 개 생성하지 않아도 된다.
-      // 객체 안에서 key를 []로 감싸면 그 안에 넣은 레퍼런스가 가리키는 실제 값이 key 값으로 활용된다.
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleClick = () => {
-    alert(this.state.usernname + ": " + this.state.message);
-    this.setState({
-      message: "",
-      usernname: "",
-    });
-  };
-
-  handleKeyPress = (e) => {
+  const onKeyDown = (e) => {
     if (e.key === "Enter") {
-      this.handleClick();
+      onClick();
     }
   };
+};
 
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자 명"
-          value={this.state.usernname}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요 ✍️"
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyPress}
-        />
-        <button onClick={this.handleClick}>확인</button>
-      </div>
-    );
-  }
-}
+return (
+  <div>
+    <h1>이벤트 연습 - 함수형 컴포넌트</h1>
+    <input
+      type="text"
+      name="username"
+      placeholder="사용자명"
+      value={username}
+      onChange={onChangeUsername}
+    />
+    <input
+      type="text"
+      name="message"
+      placeholder="텍스트를 입력해 보세요"
+      value={message}
+      onChange={onChangeMessage}
+    />
+    <button onClick={onClick}>확인</button>
+  </div>
+);
 
 export default EventPractice;
