a7be704258b8ed9ac895524e32121133ab810c3e
SUJ11V <ejqw12@naver.com>
2023-12-16 01:58:44 +0900

    [6.4.2] 배열에 데이터 추가 기능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  // 고유한 값이 없을 때만 index 값을 key로 사용
-  // 이유: index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링하지 못한다.
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용한 id
+
+  const onChange = (e) => setInputText(e.target.value);
+
+  const onClick = () => {
+    // 배열의 내장 함수 concat을 사용하여 새로운 항목을 추가한 배열 생성
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId에 +1
+    setNames(nextNames); // names 값 업데이트
+    setInputText(""); // inputText 비우기
+  };
+
+  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
