4c7658031b44567a668d116c1341a6229ee2ab8a
SUJ11V <ejqw12@naver.com>
2024-01-12 03:18:37 +0900

    [8.2.2] 특정 값이 업데이트될 때만 실행

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -6,12 +6,10 @@ const Info = () => {
 
   // useEffect: 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정
   useEffect(() => {
-    console.log("렌더링이 완료되었습니다.");
-    console.log({
-      name,
-      nickname,
-    });
-  }, []); // 두 번째 파라미터에 빈 배열을 넣어주면 마운트될 때만 실행시킬 수 있다.
+    console.log(name);
+  }, [name]);
+  // props 안에 들어 있는 value 값이 바뀔 때만 특정 작업을 수행하기 위해선
+  // 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어 주면 된다.
 
   const onChangeName = (e) => {
     setName(e.target.value);
