d80ca822222f7cffd5c0280027e352433edd3144
SUJ11V <ejqw12@naver.com>
2024-01-12 04:06:59 +0900

    [8.3.1] useReducer로 카운터 구현하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,22 +1,8 @@
 import React, { useState } from "react";
-import Info from "./Info";
+import Counter from "./Counter";
 
 function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      {/* 컴포넌트의 가시성 설정 */}
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
 }
 
 export default App;
--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+// reducer: 현재 상태와 업데이트를 위해 필요한 정보를 담은 액션 값을 전달받아 새로운 상태로 변환하는 함수
+function reducer(state, action) {
+  // acion, type에 따라 다른 작업 수행
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
 
 const Counter = () => {
-  // 기본값 0으로 설정
-  // useState 함수는 호출되면 배열을 반환 -> 1번째 원소: 상태값, 2번째 원소: 상태를 설정하는 함수
-  const [value, setValue] = useState(0);
-
+  // useReducer의 첫 번째 파라미터: reducer 함수
+  // useReducer의 두 번째 파라미터: 해당 reducer의 기본값
+  // state: 현재 가리키고 있는 상태
+  // dispatch: 액션을 발생시키는 함수
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
   return (
     <div>
       <p>
-        현재 카운터 값은 <b>{value}</b>입니다.
+        현재 카운터 값은 <b>{state.value}</b>입니다.
       </p>
-      <button onClick={() => setValue(value + 1)}>+1</button>
-      <button onClick={() => setValue(value - 1)}>-1</button>
+      {/* 함수 안에 파라미터로 액션 값을 넣어주면 reducer 함수가 호출되는 구조 */}
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
     </div>
   );
 };
