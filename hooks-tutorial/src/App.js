e158732ed613edaa73689884a7644c97e3f4e336
SUJ11V <ejqw12@naver.com>
2024-01-12 03:29:38 +0900

    [8.2.3] 뒷정리(cleanup) 함수

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Info from "./Info";
 
 function App() {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      {/* 컴포넌트의 가시성 설정 */}
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -6,10 +6,14 @@ const Info = () => {
 
   // useEffect: 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정
   useEffect(() => {
+    console.log("effect");
     console.log(name);
+    // 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면 뒷정리(cleanup) 함수를 반환해 주어야 함
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
   }, [name]);
-  // props 안에 들어 있는 value 값이 바뀔 때만 특정 작업을 수행하기 위해선
-  // 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어 주면 된다.
 
   const onChangeName = (e) => {
     setName(e.target.value);
