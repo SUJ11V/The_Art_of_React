06baffac2e192c3415bf5b7c4fee72b07f070519
SUJ11V <ejqw12@naver.com>
2024-01-20 03:44:16 +0900

    [10.3.1] useState를 사용하여 일정 관리

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      {/* 배열을 TodoList에 props로 전달하여 TodoItem으로 변환하여 렌더링 */}
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
