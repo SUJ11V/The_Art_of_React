17a136e424fe155ef28b3bf67f66a0e91d6f2168
SUJ11V <ejqw12@naver.com>
2024-01-20 15:37:33 +0900

    [10.3.3] 목록 삭제 기능 구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -36,11 +36,18 @@ const App = () => {
     nextId.current += 1; // id값 1씩 순차적으로 증가
   });
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id)); // id값을 가져와 id값에 해당되지 않는 값만 남김
+    },
+    [todos], // todos에 변화가 생겼을 때, 함수 생성
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
       {/* 배열을 TodoList에 props로 전달하여 TodoItem으로 변환하여 렌더링 */}
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 };
