dd2393170ac9aff621e797bbf70ccd8980cafd5f
SUJ11V <ejqw12@naver.com>
2024-01-22 01:31:27 +0900

    [12.1.4] immer 적용해보기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -12,10 +13,13 @@ const App = () => {
   const onChange = useCallback(
     (e) => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
+      setForm(
+        // 첫 번째 파라미터: 수정하고 싶은 상태
+        // 두 번째 파라미터: 상태를 어떻게 업데이트할지 정의하는 함수
+        produce(form, (draft) => {
+          draft[name] = value;
+        }),
+      );
     },
     [form], // form에 변화가 있을 경우에 함수 생성
   );
@@ -31,10 +35,12 @@ const App = () => {
       };
 
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        // immer를 사용할 땐 직접적인 영향을 미치는 push, splice 등의 함수를 사용해도 무방하다.
+        produce(data, (draft) => {
+          draft.array.push(info);
+        }),
+      );
 
       // form 초기화
       setForm({
@@ -50,11 +56,15 @@ const App = () => {
   // 항목을 삭제하는 함수
   const onRemove = useCallback(
     (id) => {
-      setData({
-        ...data,
-        // id가 일치하지 않으면 남김
-        array: data.array.filter((info) => info.id !== id),
-      });
+      setData(
+        // 이 경우 배열 내장 함수인 filter를 쓰는 것이 코드가 더 깔끔함
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1,
+          );
+        }),
+      );
     },
     [data],
   );
