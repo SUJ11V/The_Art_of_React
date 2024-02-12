929a901d10c58890656cbbfbf1adff373924a9ca
SUJ11V <ejqw12@naver.com>
2024-02-13 05:34:01 +0900

    [14.4] 뉴스 뷰어 UI 생성

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,39 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  // asynx: 프라미스 반환
-  const onClick = async () => {
-    try {
-      // await: 프라미스가 처리될 때까지 대기
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      ); // 가짜 API 호출
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          // 객체를 JSON 형식으로 표시
-          // 두 번째 매개변수: 문자열에 포함될 값 지정
-          // 세 번째 매개변수: 공백 설정
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
